93bf83cbf6624f27fcca4c19f4a5f2636fd420e9
mizchi <miz404@gmail.com>
2022-02-13 17:34:50 +0900

    it works

--- 7_emit/emitter.ts
+++ 7_emit/emitter.ts
@@ -1,7 +1,5 @@
-import {
-  assert,
-  equal,
-} from "https://deno.land/std@0.122.0/testing/asserts.ts";
+import { assert } from "https://deno.land/std@0.122.0/testing/asserts.ts";
+import { signedLEB128 as int, unsignedLEB128 as uint, vec } from "./utils.ts";
 
 const emptyArray = 0x0;
 
@@ -18,9 +16,9 @@ enum Op {
   i64_const = 0x42,
   f32_const = 0x43,
   f64_const = 0x44,
-
   i32_eq = 0x46,
   i32_ne = 0x47,
+  i32_add = 0x6A,
   f32_add = 0x92,
 }
 
@@ -51,81 +49,8 @@ enum Val {
   f32 = 0x7d,
 }
 
-type Binary = number | Binary[];
-
-// https://ja.wikipedia.org/wiki/IEEE_754
-// 浮動小数点に
-export function ieee754(n: number): Uint8Array {
-  // 4 byte の Uint8Array を作り、その中に IEEE754 で表現された float を書き込んで変換する
-  const buf = new ArrayBuffer(4);
-  new DataView(buf).setFloat32(0, n, true);
-  return new Uint8Array(buf);
-}
-
-Deno.test("ieee754", () => {
-  const out = ieee754(1);
-  equal(Array.from(out), [0, 0, 128, 63]);
-});
-
-// https://webassembly.github.io/spec/core/syntax/types.html#vector-types
-// vector は整数、浮動小数点によらない何らかの数値型
-function vec(data: any): number[] {
-  return [
-    // データ長
-    ...unsignedLEB128(data.length),
-    // 実体
-    ...data.flat(Infinity),
-  ];
-}
-
-export function unsignedLEB128(n: number): number[] {
-  const buffer = [];
-  do {
-    let byte = n & 0x7f;
-    n >>>= 7;
-    if (n !== 0) {
-      byte |= 0x80;
-    }
-    buffer.push(byte);
-  } while (n !== 0);
-  return buffer;
-}
-
-type BinaryArray = Array<number | BinaryArray | Uint8Array>;
-
-// https://en.wikipedia.org/wiki/LEB128
-// 整数値の内部表現
-export function signedLEB128(n: number) {
-  const buffer = [];
-  let byte: number;
-  while (true) {
-    byte = n & 0x7f;
-    n >>>= 7;
-    const v = byte & 0x40;
-    if (
-      // n が 0 で byte で次の bit がないときは終了
-      (n === 0 && v === 0) ||
-      // オーバーフローしている
-      (n === -1 && v !== 0)
-    ) {
-      buffer.push(byte);
-      return buffer;
-    }
-    byte |= 0x80;
-    buffer.push(byte);
-  }
-}
-
-Deno.test("signedLEB128", () => {
-  // console.log("xxxx", signedLEB128(64));
-  assert(equal(signedLEB128(0), [0]));
-  assert(equal(signedLEB128(1), [1]));
-  assert(equal(signedLEB128(63), [63]));
-  assert(equal(signedLEB128(64), [192, 0]));
-});
-
-const uint = unsignedLEB128;
-const int = signedLEB128;
+// type Binary = number | Binary[];
+type Binary = number[];
 
 export function encodeString(str: string): number[] {
   return [str.length, ...Array.from(str).map((s) => s.charCodeAt(0))];
@@ -136,7 +61,6 @@ type TypeExpr = [
   type: Type.Func,
   ...xs: Vec,
 ];
-type CodeExpr = Vec;
 type Ptr<T> = number & { __type__: T };
 
 // AST Builder
@@ -169,8 +93,7 @@ function emitter() {
       Section.code,
       vec(vec(_codes)),
     ];
-    // console.log(nonFlat);
-    return Uint8Array.from(nonFlat.flat());
+    return Uint8Array.from(nonFlat.flat(Infinity));
   }
 
   function _addType(expr: TypeExpr) {
@@ -191,10 +114,10 @@ function emitter() {
       Section.type
     >;
   }
-  function _func(typePtr: Ptr<Section.type>, code: CodeExpr) {
+  function _func(typePtr: Ptr<Section.type>, code: Binary) {
     const ptr = _funcs.length + _imports.length;
     _funcs.push([typePtr]);
-    _codes.push(vec(code));
+    _codes.push(vec(code).flat(Infinity));
     return ptr as Ptr<Section.func>;
   }
   function _export(name: string, funcPtr: Ptr<Section.func>) {
@@ -213,85 +136,114 @@ function emitter() {
     ]);
     return ptr as Ptr<Section.func>;
   }
+
+  function _code(stmts: Binary[]) {
+    return [
+      emptyArray,
+      ...stmts,
+      Op.end,
+    ].flat();
+  }
+
   return {
     export: _export,
     import: _import,
     func: _func,
     funcType: _funcType,
     build: _build,
+    code: _code,
   };
 }
 
 const emit = () => {
   const $ = emitter();
-  const addFuncType = $.funcType([Val.f32, Val.f32], [Val.f32]);
-  const logPtr = $.import("env", "log", $.funcType([Val.i32], []));
+  const i32_void = $.funcType([Val.i32], []);
+  const i32i32_i32 = $.funcType([Val.i32, Val.i32], [Val.i32]);
+  const logPtr = $.import("env", "log", i32_void);
 
-  const addRef = $.func(
-    addFuncType,
-    [
-      emptyArray, /** locals */
+  function i32_add(a: Binary, b: Binary) {
+    return [
+      ...a,
+      ...b,
+      Op.i32_add,
+    ];
+  }
+
+  function i32_const(n: number): Binary {
+    return [
+      Op.i32_const,
+      ...int(n),
+    ];
+  }
+
+  function local_get(local_idx: number): Binary {
+    return [
       Op.local_get,
-      ...uint(0),
-      Op.local_get,
-      ...uint(1),
-      Op.f32_add,
-      Op.end,
-    ],
+      ...uint(local_idx),
+    ];
+  }
+
+  const add = $.func(
+    i32i32_i32,
+    $.code([
+      i32_add(
+        local_get(0),
+        i32_add(
+          local_get(1),
+          i32_const(5),
+        ),
+      ),
+    ]),
   );
 
-  const idRef = $.func(
+  const id = $.func(
     $.funcType([Val.i32], [Val.i32]),
-    [
-      emptyArray, /** locals */
-      Op.local_get,
-      ...uint(0),
-      Op.end,
-    ],
-  );
-  const printPtr = $.func(
-    $.funcType([Val.i32], []),
-    [
-      emptyArray, /** locals */
-      Op.local_get,
-      ...int(0),
-      Op.call,
-      ...uint(logPtr),
-      Op.end,
-    ],
+    $.code([
+      local_get(0),
+    ]),
   );
 
-  $.export("run", addRef);
-  $.export("id", idRef);
-  $.export("print", printPtr);
+  const print = $.func(
+    i32_void,
+    $.code([
+      local_get(0),
+      [Op.call, ...uint(logPtr)],
+    ]),
+  );
+
+  $.export("add", add);
+  $.export("id", id);
+  $.export("print", print);
+
   return $.build();
+};
+
+const defaultImports: WebAssembly.Imports = {
+  env: {
+    buffer: new WebAssembly.Memory({ initial: 1 }),
+    log(data: number) {
+      console.log("[wasm:log]", data);
+    },
+  },
 };
 
 export async function compile(
   binary: Uint8Array,
+  imports: WebAssembly.Imports = defaultImports,
 ) {
-  const memory = new WebAssembly.Memory({ initial: 1 });
-  const imports: WebAssembly.Imports = {
-    env: {
-      buffer: memory,
-      log(data: number) {
-        console.log("[wasm:log]", data);
-      },
-    },
-  };
   const ret = await WebAssembly.instantiate(binary, imports);
   return ret.instance.exports as {
     [key: string]: (...args: number[]) => number;
   };
 }
 
-// const binary = emit();
+const binary = emit();
 // await Deno.writeFile("out.wasm", binary);
 
-const exports = await compile(emit());
+const exports = await compile(binary);
 
-Deno.test("run", () => {
-  assert(exports.run(4, 2) === 6, "should be 6");
+Deno.test("add", () => {
+  assert(exports.add(4, 2) === 11);
 });
 
 Deno.test("id", () => {
---
+++ 7_emit/utils.ts
@@ -0,0 +1,74 @@
+import {
+  assert,
+  equal,
+} from "https://deno.land/std@0.122.0/testing/asserts.ts";
+
+type Binary = number | Binary[];
+
+// https://ja.wikipedia.org/wiki/IEEE_754
+// 浮動小数点に
+export function ieee754(n: number): number[] {
+  // 4 byte の Uint8Array を作り、その中に IEEE754 で表現された float を書き込んで変換する
+  const buf = new ArrayBuffer(4);
+  new DataView(buf).setFloat32(0, n, true);
+  return Array.from(new Uint8Array(buf));
+}
+
+Deno.test("ieee754", () => {
+  const out = ieee754(1);
+  equal(out, [0, 0, 128, 63]);
+});
+
+// https://webassembly.github.io/spec/core/syntax/types.html#vector-types
+// vector は整数、浮動小数点によらない何らかの数値型
+export function vec(data: any): any {
+  return [
+    // データ長
+    ...unsignedLEB128(data.length),
+    ...data.flat(Infinity),
+  ];
+}
+
+export function unsignedLEB128(n: number): number[] {
+  const buffer = [];
+  do {
+    let byte = n & 0x7f;
+    n >>>= 7;
+    if (n !== 0) {
+      byte |= 0x80;
+    }
+    buffer.push(byte);
+  } while (n !== 0);
+  return buffer;
+}
+
+// https://en.wikipedia.org/wiki/LEB128
+// 整数値の内部表現
+export function signedLEB128(n: number) {
+  const buffer = [];
+  let byte: number;
+  while (true) {
+    byte = n & 0x7f;
+    n >>>= 7;
+    const v = byte & 0x40;
+    if (
+      // n が 0 で byte で次の bit がないときは終了
+      (n === 0 && v === 0) ||
+      // オーバーフローしている
+      (n === -1 && v !== 0)
+    ) {
+      buffer.push(byte);
+      return buffer;
+    }
+    byte |= 0x80;
+    buffer.push(byte);
+  }
+}
+
+Deno.test("signedLEB128", () => {
+  // console.log("xxxx", signedLEB128(64));
+  assert(equal(signedLEB128(0), [0]));
+  assert(equal(signedLEB128(1), [1]));
+  assert(equal(signedLEB128(63), [63]));
+  assert(equal(signedLEB128(64), [192, 0]));
+});
